8c1ddd1577b2dfbdd5570e7e152b5e7b3726bec5
chaoumos <18700186+chaoumos@users.noreply.github.com>
2022-07-29 00:58:22 +0100

    parser change2

--- directus.config.js
+++ directus.config.js
@@ -3,7 +3,7 @@ const parseDbUrl = require("parse-db-url");
 const dbConfig = parseDbUrl(process.env.DATABASE_URL);
 
 module.exports = {
-  DB_CLIENT: dbConfig.driver || "walo",
+  DB_CLIENT: dbConfig.adapter || "walo",
   DB_HOST: dbConfig.host || "walo",
   DB_PORT: dbConfig.port || "walo",
   DB_DATABASE: dbConfig.database || "walo",
