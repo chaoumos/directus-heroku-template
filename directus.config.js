c96b804c7dd32bab40a5da5e0b8ec9d6d3e62496
chaoumos <18700186+chaoumos@users.noreply.github.com>
2022-07-28 17:20:21 +0100

    .env modif

--- directus.config.js
+++ directus.config.js
@@ -1,4 +1,15 @@
+// Heroku to Directus mappings
+const parseDbUrl = require("parse-database-url");
+const dbConfig = parseDbUrl(process.env.DATABASE_URL);
+
 module.exports = {
+  DB_CLIENT: parseDbUrl(process.env.DATABASE_URL).driver || "walo",
+  DB_HOST: parseDbUrl(process.env.DATABASE_URL).host || "walo",
+  DB_PORT: parseDbUrl(process.env.DATABASE_URL).port || "walo",
+  DB_DATABASE: parseDbUrl(process.env.DATABASE_URL).database || "walo",
+  DB_USER: parseDbUrl(process.env.DATABASE_URL).user || "walo",
+  DB_PASSWORD: parseDbUrl(process.env.DATABASE_URL).password,
+
   DB_CONNECTION_STRING:
     process.env.DB_CONNECTION_STRING || process.env.DATABASE_URL,
   RATE_LIMITER_REDIS: process.env.RATE_LIMITER_REDIS || process.env.REDIS_URL,
@@ -12,3 +23,12 @@ module.exports = {
   EMAIL_SMTP_PASSWORD:
     process.env.EMAIL_SMTP_PASSWORD || process.env.MAILGUN_SMTP_PASSWORD,
 };
+
+// {
+//   "driver": "mysql",
+//   "user": "root",
+//   "password": "password123",
+//   "host": "localhost",
+//   "port": "3306",
+//   "database": "app_db"
+// }
