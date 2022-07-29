8996892fdb3cb6f99bfc0d0d804ab81debfdd0b9
chaoumos <18700186+chaoumos@users.noreply.github.com>
2022-07-29 23:26:11 +0100

    parser change99

---
+++ directus-config.js
@@ -0,0 +1,34 @@
+// Heroku to Directus mappings
+const parseDbUrl = require("parse-db-url");
+const dbConfig = parseDbUrl(process.env.DATABASE_URL);
+
+module.exports = {
+  DB_CLIENT: process.env.DB_CLIENT || "pg",
+  DB_HOST: dbConfig.host,
+  DB_PORT: dbConfig.port,
+  DB_DATABASE: dbConfig.database,
+  DB_USER: dbConfig.user,
+  DB_PASSWORD: dbConfig.password,
+  DB_CONNECTION_STRING:
+    process.env.DB_CONNECTION_STRING || process.env.DATABASE_URL,
+  RATE_LIMITER_REDIS: process.env.RATE_LIMITER_REDIS || process.env.REDIS_URL,
+  CACHE_REDIS: process.env.CACHE_REDIS || process.env.REDIS_URL,
+  EMAIL_FROM: process.env.EMAIL_FROM || process.env.MAILGUN_SMTP_LOGIN,
+  EMAIL_SMTP_HOST:
+    process.env.EMAIL_SMTP_HOST || process.env.MAILGUN_SMTP_SERVER,
+  EMAIL_SMTP_PORT: process.env.EMAIL_SMTP_PORT || process.env.MAILGUN_SMTP_PORT,
+  EMAIL_SMTP_USER:
+    process.env.EMAIL_SMTP_USER || process.env.MAILGUN_SMTP_LOGIN,
+  EMAIL_SMTP_PASSWORD:
+    process.env.EMAIL_SMTP_PASSWORD || process.env.MAILGUN_SMTP_PASSWORD,
+};
+
+//parsed url object
+// {
+//   "driver": "mysql",
+//   "user": "root",
+//   "password": "password123",
+//   "host": "localhost",
+//   "port": "3306",
+//   "database": "app_db"
+// }
